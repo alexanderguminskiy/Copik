1eaf05380293d4ca386368896e4d63d16b8d226a
AlexWork <alexsguminskiy@gmail.com>
2020-12-10 17:39:29 +0300

    create modal and mailler

---
+++ src/js/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+    //Modal
+
+    $('[data-modal="consultation"]').on('click', function() {
+        $('.overlay, #consultation').fadeIn();
+    });
+
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks').fadeOut();
+    });
+
+    //MaskedInput
+    
+    $('input[name=phone]').mask("+375(99) 999-99-99");
+
+    //SendToEmail
+
+    $('form').submit( function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+});
