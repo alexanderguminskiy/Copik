3171841cabc71d4eacc793df40fa63313f6733d3
AlexWork <alexsguminskiy@gmail.com>
2020-12-16 15:14:36 +0300

    site done

---
+++ dist/js/script.js
@@ -0,0 +1,76 @@
+$(document).ready(function(){
+
+    //Modal
+
+    $('[data-modal="consultation"]').on('click', function() {
+        $('.overlay, #consultation').fadeIn();
+    });
+
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks').fadeOut();
+    });
+
+    //MaskedInput
+    
+    $('input[name=phone]').mask("+375(99) 999-99-99");
+
+    //SendToEmail
+
+    $('form').submit( function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #feedback').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+    //carousel 
+
+    $('.carousel').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/left.png" alt="left"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/right.png" alt="right"></button>',
+        infinite: true,
+        slidesToShow: 1,
+        adaptiveHeight: true
+    });
+
+    //pageup 
+
+    $(window).scroll(function() {
+        if($(this).scrollTop() > 1800) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }   
+    });
+
+    //humburger
+
+    const menu = document.querySelector('.promo__header'),
+    menuItem = document.querySelectorAll('.promo__header'),
+    hamburger = document.querySelector('.humburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('humburger_active');
+        menu.classList.toggle('promo__header_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('humburger_active');
+            menu.classList.toggle('promo__header_active');
+        })
+    })
+
+    //WOW JS
+
+    new WOW().init();
+
+});
--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,14 @@ const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
 const cleanCSS = require('gulp-clean-css');
+const htmlmin = require('gulp-htmlmin');
+const imagemin = require('gulp-imagemin');
 
 // Static server
 gulp.task('server', function() {
     browserSync.init({
         server: {
-            baseDir: "src"
+            baseDir: "dist"
         }
     });
 });
@@ -20,13 +22,47 @@ gulp.task('styles', function () {
       .pipe(autoprefixer({cascade: false}))
       .pipe(rename({suffix: ".min"}))
       .pipe(cleanCSS({compatibility: 'ie8'}))
-      .pipe(gulp.dest('src/css'))
+      .pipe(gulp.dest('dist/css'))
       .pipe(browserSync.stream());
   });
+
+gulp.task('html', () => {
+return gulp.src('src/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('img', () => {
+    return gulp.src('src/img/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'))
+});
+
+gulp.task('icons', () => {
+    return gulp.src('src/icons/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/icons'))
+});
+
+gulp.task('fonts', () => {
+    return gulp.src('src/fonts/*')
+        .pipe(gulp.dest('dist/fonts'))
+});
+
+gulp.task('js', () => {
+    return gulp.src('src/js/*')
+        .pipe(gulp.dest('dist/js'))
+});
+
+gulp.task('mailer', () => {
+    return gulp.src('src/mailer/**/*')
+        .pipe(gulp.dest('dist/mailer'))
+});
+
 
 gulp.task('watch', function() {
     gulp.watch('src/sass/**/*.scss', gulp.parallel('styles'));
     gulp.watch('src/*.html').on("change", browserSync.reload);
 });
 
-gulp.task('default', gulp.parallel('server', 'styles', 'watch'));
+gulp.task('default', gulp.parallel('server', 'watch', 'html', 'fonts', 'img', 'icons', 'styles', 'js', 'mailer'));
